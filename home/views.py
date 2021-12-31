170c5d2400e03b628153a609d043674b365244b8
OliverDaviesCodes <oliverdavieswork@gmail.com>
2021-12-31 10:31:32 +0000

    Updated the context processor to generate bag amounts

--- home/views.py
+++ home/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def view_bag(request):
-    """ View that renders the bag contents page """
-    return render(request, 'bag/bag.html')
+def index(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/index.html')
+
