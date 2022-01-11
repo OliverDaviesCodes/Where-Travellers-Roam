d3a0bc3c68a9d0ee5bf52735bbd6b39956cd0240
OliverDaviesCodes <oliverdavieswork@gmail.com>
2022-01-11 10:05:46 +0000

    Added Stripe elements to website and payment functions

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KGfmxI5ZIgRiZR4ldmoQreYWBJ9VurJC0xz4gb5Y4kH9KowjGmwp5rSGvJQhu0Zg6XPQFNz5GxSnksPjezWw0dj002eqUNzg0',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
