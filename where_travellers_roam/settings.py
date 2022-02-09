150a8adbf1eddc65ce6eb2326d03f2de3116c157
OliverDaviesCodes <oliverdavieswork@gmail.com>
2022-02-09 11:57:03 +0000

    Removed secret key and set debug

--- where_travellers_roam/settings.py
+++ where_travellers_roam/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b=z10x$$v5y809dwxkin+=0@k+e_ucmxzt(rl4jz8o!%757b*x'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['where-travelers-roam.herokuapp.com', 'localhost']
 
