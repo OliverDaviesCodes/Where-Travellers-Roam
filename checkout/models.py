445168fef4f24573ce67566c3db51b187aacb11f
OliverDaviesCodes <oliverdavieswork@gmail.com>
2022-03-08 11:17:39 +0000

    Styled bag app, updated checkout app and added json db file

--- checkout/models.py
+++ checkout/models.py
@@ -43,13 +43,6 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        # self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
-        # if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-        #     self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
-        # else:
-        #     self.delivery_cost = 0
-        # self.grand_total = self.order_total + self.delivery_cost
-        # self.save()
 
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total > settings.DISCOUNT_THRESHOLD:
@@ -58,11 +51,7 @@ class Order(models.Model):
             self.grand_total = self.order_total
         self.save()
 
-        # self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
-        # if self.order_total >= DISCOUNT_THRESHOLD:
-        #     self.grand_total = self.order_total - 0.1*DISCOUNT_THRESHOLD
-        # self.save()
-
+        
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the order number
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -11,11 +11,9 @@ import stripe
 @csrf_exempt
 def webhook(request):
     """Listen for webhooks from Stripe"""
-    # Setup
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
